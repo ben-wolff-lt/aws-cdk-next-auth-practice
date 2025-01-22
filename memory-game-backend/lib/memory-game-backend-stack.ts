d9c39ba97c1c36282328330763c2db8e7feb939d
Ben Wolff <WolffBenjamin@JohnDeere.com>
2025-01-22 11:20:55 -0600

    aws infra setup

---
+++ memory-game-backend/lib/memory-game-backend-stack.ts
@@ -0,0 +1,76 @@
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { Construct } from 'constructs';
+
+export class MemoryGameBackendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create the DynamoDB Table
+    const gameTable = new dynamodb.Table(this, 'GameTable', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST
+    });
+
+    // Add GSI for Leaderboard
+    gameTable.addGlobalSecondaryIndex({
+      indexName: 'GSI_Leaderboard',
+      partitionKey: { name: 'GSI_PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'GSI_SK', type: dynamodb.AttributeType.NUMBER },
+      projectionType: dynamodb.ProjectionType.ALL // Include all attributes in GSI
+    });
+
+    // Define Lambdas
+    const saveGameResultLambda = new lambda.Function(
+      this,
+      'SaveGameResultLambda',
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'saveGameResult.handler',
+        code: lambda.Code.fromAsset('lambdas'), // Directory containing Lambda code
+        environment: {
+          TABLE_NAME: gameTable.tableName
+        }
+      }
+    );
+
+    const getLeaderboardLambda = new lambda.Function(
+      this,
+      'GetLeaderboardLambda',
+      {
+        runtime: lambda.Runtime.NODEJS_18_X,
+        handler: 'getLeaderboard.handler',
+        code: lambda.Code.fromAsset('lambdas'),
+        environment: {
+          TABLE_NAME: gameTable.tableName
+        }
+      }
+    );
+
+    // Grant Permissions
+    gameTable.grantReadWriteData(saveGameResultLambda);
+    gameTable.grantReadData(getLeaderboardLambda);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'MemoryGameApi', {
+      restApiName: 'Memory Game Service'
+    });
+
+    // Save Game Result Route
+    const gameResultResource = api.root.addResource('game-result');
+    gameResultResource.addMethod(
+      'POST',
+      new apigateway.LambdaIntegration(saveGameResultLambda)
+    );
+
+    // Get Leaderboard Route
+    const leaderboardResource = api.root.addResource('leaderboard');
+    leaderboardResource.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getLeaderboardLambda)
+    );
+  }
+}
